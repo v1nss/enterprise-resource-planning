2b6e5b19724a38ba6a89a1f7e0714f22658e1e5c
shew <sherwinmendez05@gmail.com>
2024-01-22 17:59:13 +0800

    can compute price and qty

--- src/components/SupplyChain/InventoryManagement.js
+++ src/components/SupplyChain/InventoryManagement.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 
 import { FiTrash } from "react-icons/fi";
 import { FiCheck } from "react-icons/fi";
@@ -6,49 +6,59 @@ import { FiEdit2 } from "react-icons/fi";
 
 const InventoryManagement = () => {
   const [allExpense, setAllExpense] = useState([]);
-  const [newExpense, setNewExepense] = useState("");
+  const [newExpense, setNewExpense] = useState("");
   const [newQuantity, setNewQuantity] = useState("");
   const [newPrice, setNewPrice] = useState("");
-  const [completedTodos, setCompletedTodos] = useState([]);
-  const [isCompletedScreen, setIsCompletedScreen] = useState(false);
+  const [, setNewTotalPrice] = useState("");
   const [, setIsEditingTodo] = useState(false);
-  const [newEditedTodoTitle, setNewEditedTodoTitle] = useState("");
-  const [newEditedDescription, setNewEditedDescription] = useState("");
-  const [editingTodoIndex, setEditingTodoIndex] = useState(null);
 
+  const handleSum = () => {
+    let updatedExpenseArr = [...allExpense];
+    let totalNewPrice = updatedExpenseArr.reduce(
+      (total, expense) => total + parseFloat(expense.totalprice),
+      0
+    );
+    console.log(totalNewPrice);
+  };
+  
   const handleAddNewExpense = () => {
-    let newToDoObj = {
+    const price = parseFloat(newPrice);
+    const quantity = parseFloat(newQuantity);
+  
+    const newTotalPrice = isNaN(price) || isNaN(quantity) ? null : price * quantity;
+    let newProductListObj = {
       title: newExpense,
-      description: newPrice,
+      price: newPrice,
+      quantity: newQuantity,
+      totalprice: newTotalPrice,
     };
     if (newExpense.trim() !== "") {
-      let updatedTodoArr = [...allExpense];
-      updatedTodoArr.push(newToDoObj);
-      setAllExpense(updatedTodoArr);
-      localStorage.setItem("todolist", JSON.stringify(updatedTodoArr));
+      let updatedProductList = [...allExpense];
+      updatedProductList.push(newProductListObj);
+      setAllExpense(updatedProductList);
+      console.log(updatedProductList);
+      localStorage.setItem("expenselist", JSON.stringify(updatedProductList));
       setNewPrice("");
       setNewQuantity("");
-      setNewExepense("");
+      setNewExpense("");
+      setNewTotalPrice(""); 
+      
     } else {
-      alert("Title cannot be empty. Please enter a title.");
+      alert("Title cannot be empty. Please enter product name.");
     }
+      handleSum();
   };
 
   const handleInventoryDelete = (index) => {
     let reducedTodos = [...allExpense];
     reducedTodos.splice(index, 1);
-    localStorage.setItem("todolist", JSON.stringify(reducedTodos));
+    localStorage.setItem("expenselist", JSON.stringify(reducedTodos));
     setAllExpense(reducedTodos);
   };
 
-  const handleSum = () => {
-    let updatedTodoArr = [...allExpense];
-    let totalNewPrice = updatedTodoArr.reduce(
-      (total, todo) => total + parseFloat(todo.price),
-      0
-    );
-    console.log(totalNewPrice);
-  };
+  const handleCompute = () => {
+
+  }
 
   return (
     <div>
@@ -58,17 +68,8 @@ const InventoryManagement = () => {
           <input
             type="text"
             value={newExpense}
-            onChange={(e) => setNewExepense(e.target.value)}
-            placeholder="What's the title of your To Do?"
-          />
-        </div>
-        <div className="inventory-input-item">
-          <label>Quantity:</label>
-          <input
-            type="text"
-            value={newExpense}
-            onChange={(e) => setNewQuantity(e.target.value)}
-            placeholder="What's the title of your To Do?"
+            onChange={(e) => setNewExpense(e.target.value)}
+            placeholder="Product"
           />
         </div>
         <div className="inventory-input-item">
@@ -77,14 +78,26 @@ const InventoryManagement = () => {
             type="number"
             value={newPrice}
             onChange={(e) => setNewPrice(e.target.value)}
-            placeholder="What's the description of your To Do?"
+            placeholder="Price"
+          />
+        </div>
+        <div className="inventory-input-item">
+          <label>Quantity:</label>
+          <input
+            type="number"
+            value={newQuantity}
+            onChange={(e) => setNewQuantity(e.target.value)}
+            placeholder="Quantity"
           />
         </div>
         <div className="inventory-input-item">
           <button
             className="primary-btn"
             type="button"
-            onClick={handleAddNewExpense}
+            onClick={() => {
+              handleAddNewExpense();
+              
+            }}
           >
             Add
           </button>
@@ -96,8 +109,10 @@ const InventoryManagement = () => {
           {allExpense.map((item, index) => (
             <div>
               <div>
-                <h3>{item.title}</h3>
-                <p>{item.price}</p>
+                <h3>product: {item.title}</h3>
+                <p>price: {item.price}</p>
+                <p>quantity: {item.quantity}</p>
+                <p>totalprice: {item.totalprice}</p>
               </div>
               <div className="inventory-list-buttons">
                 <FiTrash
@@ -110,14 +125,8 @@ const InventoryManagement = () => {
                   className="icon"
                   onClick={() => {
                     setIsEditingTodo(true);
-                    setEditingTodoIndex(index);
                   }}
                 />
-                {/* <FiCheck
-                  title="Completed?"
-                  className="check-icon"
-                  onClick={() => handleComplete(index)}
-                /> */}
               </div>
             </div>
           ))}
