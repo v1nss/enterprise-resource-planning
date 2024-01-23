058615f1e0874ee4360006c1607b7cb61169cd9d
vince <vincemarc.mr@gmail.com>
2024-01-23 23:11:43 +0800

    projectmanagement

--- src/pages/ProjectManagement.js
+++ src/pages/ProjectManagement.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 import {
     Link,
@@ -9,12 +9,40 @@ import ProjectBudgeting from "../components/ProjectManagement/ProjectBudgeting";
 import TaskManagement from "../components/ProjectManagement/TaskManagement";
 
 const ProjectManagement = () => {
+
+  const [showPlanningScheduling, setShowPlanningScheduling] = useState(false);
+  const [showProjectBudgeting, setShowProjectBudgeting] = useState(false);
+  const [showTaskManagement, setShowTaskManagement] = useState(false);
+
+  const handlePlanningSchedulingClick = () => {
+    setShowPlanningScheduling(true);
+    setShowProjectBudgeting(false);
+    setShowTaskManagement(false);
+  };
+
+  const handleProjectBudgetingClick = () => {
+    setShowPlanningScheduling(false);
+    setShowProjectBudgeting(true);
+    setShowTaskManagement(false);
+  };
+
+  const handleTaskManagementClick = () => {
+    setShowPlanningScheduling(false);
+    setShowProjectBudgeting(false);
+    setShowTaskManagement(true);
+  };
+
     return (
       <div>
-        <h1>Project Management</h1>
-        <PlanningScheduling />
-        <ProjectBudgeting />
-        <TaskManagement />
+      <div>
+        <button onClick={handlePlanningSchedulingClick}>Employee Information</button>
+        <button onClick={handleProjectBudgetingClick}>Time Attendance</button>
+        <button onClick={handleTaskManagementClick}>Payroll Processing</button>
+      </div>
+
+      {showPlanningScheduling && <PlanningScheduling />}
+      {showProjectBudgeting && <ProjectBudgeting />}
+      {showTaskManagement && <TaskManagement />}
       </div>
       
     );
